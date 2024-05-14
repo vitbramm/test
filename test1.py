9a2d7374a1075a867666aa2bf3f95da1edebde5b
vitbramm <142585083+vitbramm@users.noreply.github.com>
2024-05-14 14:49:37 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
-test1
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Мое первое Streamlit приложение')
+
+st.write("Вот таблица данных:")
+data = pd.DataFrame({
+    'Переменная A': np.random.randn(10),
+    'Переменная B': np.random.randn(10)
+})
+st.write(data)
+
+if st.button('Показать график'):
+    st.line_chart(data)
